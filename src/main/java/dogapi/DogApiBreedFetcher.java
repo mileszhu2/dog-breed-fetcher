4322640fa009f379ad64fa8d94521708c12b1005
mileszhu2 <miles.zhu@mail.utoronto.ca>
2025-10-16 14:39:54 -0400

    test fix

--- src/main/java/dogapi/DogApiBreedFetcher.java
+++ src/main/java/dogapi/DogApiBreedFetcher.java
@@ -42,7 +42,7 @@ public class DogApiBreedFetcher implements BreedFetcher {
                 subBreeds.add(jsonArray.getString(i));
             }
         }
-        catch(BreedNotFoundException | IOException e) {System.out.println(e.getMessage());}
-        return List.of("error");
+        catch(IOException e) {System.out.println(e.getMessage());}
+        return subBreeds;
     }
 }
